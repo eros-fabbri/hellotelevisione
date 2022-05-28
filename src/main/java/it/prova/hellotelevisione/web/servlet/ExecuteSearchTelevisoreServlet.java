a4bf10f6a76767b025b642ef6b20ed42016a3aaa
erosfabbridev <erosfabbridev@gmail.com>
2022-05-28 17:24:24 +0200

    searchForm + servlet

---
+++ src/main/java/it/prova/hellotelevisione/web/servlet/ExecuteSearchTelevisoreServlet.java
@@ -0,0 +1,36 @@
+package it.prova.hellotelevisione.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.prova.hellotelevisione.service.MyServiceFactory;
+
+@WebServlet("/ExecuteSearchTelevisoreServlet")
+public class ExecuteSearchTelevisoreServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String destinazione = null;
+
+		String marcaInput = request.getParameter("marcaInput");
+		String modelloInput = request.getParameter("modelloInput");
+
+		if (marcaInput == "" && modelloInput == "") {
+			String messaggioDaInviareAPagina = "Attenzione! E' necessario valorizzare almeno un campo.";
+			request.setAttribute("messaggioDiErrore", messaggioDaInviareAPagina);
+			destinazione = "searchForm.jsp";
+		} else {
+			request.setAttribute("listaTelevisori",
+					MyServiceFactory.getAbitanteServiceInstance().cercaPerMarcaEModello(marcaInput, modelloInput));
+			destinazione = "results.jsp";
+		}
+
+	}
+
+}
--- src/main/webapp/searchForm.jsp
+++ src/main/webapp/searchForm.jsp
@@ -7,6 +7,18 @@
 <title>Ricerca televisori</title>
 </head>
 <body>
-
+	<% if(request.getAttribute("messaggioDiErrore") != null){ %>
+	<p style="color: red;"><%=request.getAttribute("messaggioDiErrore") %></p>
+	<%  }else{ %>
+	Inserire dati da ricercare
+	<br>
+	<%}    %>
+	<form action="ExecuteSearchTelevisoreServlet" method="post">
+		<label for="marcaInputId">MARCA:</label><br> <input type="text"
+			name="marcaInput" id="marcaInputId"> <br> <label
+			for="modelloInputId">MODELLO:</label><br> <input type="text"
+			name="modelloInput"id="modelloInputId"> <br>
+		<br> <input type="submit" value="CERCA">
+	</form>
 </body>
 </html>
